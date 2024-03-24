9a879d20b50ec7901197a733adffa417ccf2f2a4
Joshua Hartle <126029939+JHartle826@users.noreply.github.com>
2024-03-24 13:01:48 -0500

    Create Website.js
    Creation of a basic JavaScript file

---
+++ Website.js
@@ -0,0 +1,8 @@
+// Custom JavaScript
+
+// Example JavaScript for a simple interactive feature
+$(document).ready(function() {
+  $(".btn-click-me").click(function() {
+    alert("Button clicked!");
+  });
+});
